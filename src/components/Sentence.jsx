f873cec6cd61102c16f5bab536fc2157b763a418
Kamisia <kamila.samczuk@gmail.com>
2024-02-28 13:33:46 +0100

    modified:   src/components/Sentence.jsx modified:   src/index.css

--- src/components/Sentence.jsx
+++ src/components/Sentence.jsx
@@ -2,6 +2,9 @@ import { useGlobalContext } from "../Context";
 
 const Sentence = () => {
   const { quote, author } = useGlobalContext();
+  if (quote == "") {
+    return <span class="loader"></span>;
+  }
   return (
     <div className="sentence-container">
       <h1 className="quote">"{quote}"</h1>
