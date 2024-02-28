bfbdb67b8d7d1511eb863d66bb5a918729f5d47a
Kamisia <kamila.samczuk@gmail.com>
2024-02-28 12:36:19 +0100

    modified:   src/App.jsx 	modified:   src/Context.jsx 	modified:   src/components/Button.jsx 	modified:   src/components/Sentence.jsx 	modified:   src/index.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
+import { useGlobalContext } from "../Context";
+
 const Button = () => {
+  const { handleClick, isOpen } = useGlobalContext;
   return (
     <div>
-      <button className="btn">Your Quote Of The Day</button>
+      <button onClick={handleClick} className="btn">
+        Your Quote Of The Day
+      </button>
     </div>
   );
 };
