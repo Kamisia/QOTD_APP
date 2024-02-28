52f5b91993517f040cc804af9bfdce072072ba96
Kamisia <kamila.samczuk@gmail.com>
2024-02-28 13:12:18 +0100

    modified:   src/App.jsx 	modified:   src/Context.jsx 	modified:   src/components/Button.jsx
    (cherry picked from commit 3685965daf14914dee4f71a9898d206a80ee7da1)

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,31 +3,33 @@ import axios from "axios";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  ///FetchData
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+  const [showSentence, setShowSentence] = useState(false);
+
+  const handleClick = () => {
+    setShowSentence(true);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("https://api.quotable.io/random");
         setQuote(response.data.content);
+
         setAuthor(response.data.author);
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
     };
 
-    fetchData();
-  }, []);
+    if (showSentence) {
+      fetchData();
+    }
+  }, [showSentence]);
 
-  //obsługa przycisku
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(true);
-  };
   return (
-    <AppContext.Provider value={{ quote, author, handleClick, isOpen }}>
+    <AppContext.Provider value={{ quote, author, handleClick, showSentence }}>
       {children}
     </AppContext.Provider>
   );
